14ca672d160b52d77a6129bc8d0f587792183d98
sarahrad17 <sarahradway@gmail.com>
2020-02-02 23:32:27 -0500

    fix notes

--- lunAR_lion/Assets/Speech.cs
+++ lunAR_lion/Assets/Speech.cs
@@ -5,96 +5,75 @@ using Microsoft.CognitiveServices.Speech;
 public class Speech : MonoBehaviour
 {
     // Hook up the two properties below with a Text and Button object in your UI.
-    public Text outputText;
-    public Button startRecoButton;
 
     private object threadLocker = new object();
     private bool waitingForReco;
     private string message;
+    public int yeet = 3;
 
     private bool micPermissionGranted = false;
 
-    public async void ButtonClick()
+    public async void SpeechContinuousRecognitionAsync()
     {
+        System.IO.File.Create(@"notepad.txt").Close();
+
         // Creates an instance of a speech config with specified subscription key and service region.
-        // Replace with your own subscription key and service region (e.g., "westus").
         var config = SpeechConfig.FromSubscription("82b1859945464df6a90737eef58dc46f", "westus");
 
-        // Make sure to dispose the recognizer after use!
+        // Creates a speech recognizer from microphone.
         using (var recognizer = new SpeechRecognizer(config))
         {
-            lock (threadLocker)
-            {
-                waitingForReco = true;
-            }
+            // Subscribes to events.
+            recognizer.Recognizing += (s, e) => {
+                var result = e.Result;
+                message = result.Reason.ToString();
+            };
 
-            // Starts speech recognition, and returns after a single utterance is recognized. The end of a
-            // single utterance is determined by listening for silence at the end or until a maximum of 15
-            // seconds of audio is processed.  The task returns the recognition text as result.
-            // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
-            // shot recognition like command or query.
-            // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
-            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+            recognizer.Recognized += (s, e) => {
+                var result = e.Result;
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    message = result.Text;
+                    System.IO.File.AppendAllText(@"notepad.txt", message);
+                    System.IO.File.AppendAllText(@"notepad.txt", "\n");
+                }
+            };
 
-            // Checks result.
-            string newMessage = string.Empty;
-            if (result.Reason == ResultReason.RecognizedSpeech)
-            {
-                newMessage = result.Text;
-            }
-            else if (result.Reason == ResultReason.NoMatch)
-            {
-                newMessage = "NOMATCH: Speech could not be recognized.";
-            }
-            else if (result.Reason == ResultReason.Canceled)
-            {
-                var cancellation = CancellationDetails.FromResult(result);
-                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
-            }
+            recognizer.Canceled += (s, e) => {
+                message = $"\n    Recognition Canceled. Reason: {e.Reason.ToString()}, CanceledReason: {e.Reason}";
+                System.IO.File.AppendAllText(@"notepad.txt", message);
+                System.IO.File.AppendAllText(@"notepad.txt", "\n");
+            };
 
-            lock (threadLocker)
+            recognizer.SessionStarted += (s, e) => {
+                message = "\n    Session started event.";
+            };
+
+            recognizer.SessionStopped += (s, e) => {
+                message = "\n    Session stopped event.";
+                System.IO.File.WriteAllText(@"notepad.txt", message);
+                System.IO.File.AppendAllText(@"notepad.txt", "\n");
+            };
+
+            // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+            await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+            
+            do
             {
-                message = newMessage;
-                waitingForReco = false;
-            }
+            
+                message = "";
+            } while (yeet == 3);
+            // Stops recognition.
+            await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
         }
     }
+
 
     void Start()
     {
-        if (outputText == null)
-        {
-            UnityEngine.Debug.LogError("outputText property is null! Assign a UI Text element to it.");
-        }
-        else if (startRecoButton == null)
-        {
-            message = "startRecoButton property is null! Assign a UI Button to it.";
-            UnityEngine.Debug.LogError(message);
-        }
-        else
-        {
-            // Continue with normal initialization, Text and Button objects are present.
-
-            micPermissionGranted = true;
-            message = " ";
-            startRecoButton.onClick.AddListener(ButtonClick);
-        }
-    }
-
-    void Update()
-    {
-        lock (threadLocker)
-        {
-            if (startRecoButton != null)
-            {
-                startRecoButton.interactable = !waitingForReco && micPermissionGranted;
-            }
-            if (outputText != null)
-            {
-                outputText.text = message;
-                System.IO.File.Create(@"notepad.txt").Close();
-                System.IO.File.WriteAllText( @"notepad.txt" , message);
-            }
-        }
+        // Continue with normal initialization, Text and Button objects are present.
+        micPermissionGranted = true;
+        message = " ";
+        SpeechContinuousRecognitionAsync();    
     }
 }
