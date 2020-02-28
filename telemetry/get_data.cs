cfb9a18ea1861c9a7f496cf4bfa5a25c7029f57f
BuildTools <unconfigured@null.spigotmc.org>
2020-02-28 17:44:38 -0500

    Tanner is a good person

--- telemetry/get_data.cs
+++ telemetry/get_data.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 //Request library
 using System.Net;
 using System.IO;
+using System.Security.Authentication;
 //using Newtonsoft.Json;
 
 namespace HelloWorld
@@ -13,19 +14,21 @@ namespace HelloWorld
     class Hello {
         static void Main(string[] args)
         {
-           string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
+            const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+            const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+            ServicePointManager.SecurityProtocol = Tls12;
+            string url = @"http://localhost:3000/api/suit";
 
            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
            request.AutomaticDecompression = DecompressionMethods.GZip;
 
-           using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-           using (Stream stream = response.GetResponseStream())
-           using (StreamReader reader = new StreamReader(stream))
-           {
-               html = reader.ReadToEnd();
-           }
-           Console.WriteLine(html);  
-    
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            { 
+                string html = reader.ReadToEnd();
+                Console.WriteLine(html);  
+            }
         }
     }
 }
