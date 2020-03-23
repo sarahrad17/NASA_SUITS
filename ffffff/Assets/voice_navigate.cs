25026fe043350753d44f96cd575a84c03be9752c
sarahrad17 <sarahradway@gmail.com>
2020-03-23 19:48:14 -0400

    add new unity tings

---
+++ ffffff/Assets/voice_navigate.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using Microsoft.CognitiveServices.Speech;
+using System.Text.RegularExpressions;
+public class voice_navigate : MonoBehaviour
+{
+
+
+    private object threadLocker = new object();
+    private bool waitingForReco;
+    private string message;
+    public int yeet = 3;
+    public int counter = 0;
+    private bool micPermissionGranted = false;
+    public bool menu_open;
+    public GameObject Menu_Buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Continue with normal initialization, Text and Button objects are present.
+        micPermissionGranted = true;
+        message = " ";
+        menu_open = false;
+        Menu_Buttons.SetActive(false);
+        //System.IO.File.Create(@"speech_output.txt").Close();
+        System.IO.File.Create(@"speech_finaloutput.txt").Close();
+        SpeechContinuousRecognitionAsync();
+    }
+
+    public async void SpeechContinuousRecognitionAsync()
+    {
+        //System.IO.File.AppendAllText(@"speech_output.txt", "starting \n");
+        // Creates an instance of a speech config with specified subscription key and service region.
+        var config = SpeechConfig.FromSubscription("82b1859945464df6a90737eef58dc46f", "westus");
+        //System.IO.File.AppendAllText(@"speech_output.txt", "config good \n");
+        // Creates a speech recognizer from microphone.
+        using (var recognizer = new SpeechRecognizer(config))
+        {
+            // Subscribes to events.
+            recognizer.Recognizing += (s, e) => {
+                var result = e.Result;
+                message = result.Reason.ToString();
+            };
+
+            recognizer.Recognized += (s, e) => {
+                var result = e.Result;
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    message = result.Text;
+                    System.IO.File.AppendAllText(@"speech_output.txt", "message");
+                    System.IO.File.AppendAllText(@"speech_output.txt", message);
+                    System.IO.File.AppendAllText(@"speech_output.txt", "\n");
+                }
+            };
+
+            recognizer.Canceled += (s, e) => {
+                message = $"\n    Recognition Canceled. Reason: {e.Reason.ToString()}, CanceledReason: {e.Reason}";
+            };
+            recognizer.SessionStarted += (s, e) => {
+                message = "\n    Session started event.";
+            };
+
+            recognizer.SessionStopped += (s, e) => {
+                message = "\n    Session stopped event.";
+            };
+
+            // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+            await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+            do
+            {
+                message = "";
+            } while (yeet == 3);
+            // Stops recognition.
+            await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if not 15th frame
+        if (counter < 150)
+        {
+            System.IO.File.AppendAllText(@"speech_finaloutput.txt", "hello");
+            counter = counter + 1;
+        }
+        //if 15th frame
+        else
+        {
+            //see if input file exists yet
+            if (File.Exists(@"speech_output.txt"))
+            {
+                //read input into array of lines
+                string[] file_contents = System.IO.File.ReadAllLines(@"speech_output.txt");
+
+                foreach (string f in file_contents)
+                {
+                    System.IO.File.AppendAllText(@"speech_finaloutput.txt", f);
+                    if (f.Contains("open menu"))
+                    {
+                        //System.IO.File.AppendAllText(@"speech_finaloutput.txt", f);
+                        if (menu_open == false)
+                        {
+                            Menu_Buttons.SetActive(true);
+                            menu_open = true;
+                        }
+                    }
+                    if (f.Contains("close menu"))
+                    {
+                        if (menu_open == true)
+                        {
+                            Menu_Buttons.SetActive(false);
+                            menu_open = false;
+                        }
+                    }
+                }
+            }
+            //System.IO.File.Create(@"speech_finaloutput.txt").Close();
+            counter = 0;
+        }
+    }
+}
