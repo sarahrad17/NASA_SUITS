851b3e4ff201c5f85e0340f80dd8f72781315204
sarahrad17 <sarahradway@gmail.com>
2020-03-24 20:15:38 -0400

    add intro hololens stuff

---
+++ ffffff/Assets/open_close_menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_close_menu : MonoBehaviour
+{
+    public bool Menu_Open;
+    public GameObject Menu;
+    void Start()
+    {
+        Menu.SetActive(false);
+        Menu_Open = false; 
+    }
+    void Update()
+    {
+        if (Menu_Open != true)
+        {
+            Menu.SetActive(false);
+        }
+        if (Menu_Open != false)
+        {
+            Menu.SetActive(true);
+        }
+    }
+}
