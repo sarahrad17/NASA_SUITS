11f00a664d4745dacf1f445cdaea41a1c20d271e
sarahrad17 <sarahradway@gmail.com>
2020-04-06 09:42:12 -0400

    run telemetry working

---
+++ ffffff/Assets/get_telemetry.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Request library
+using System.Net;
+using System.IO;
+
+public class get_telemetry : MonoBehaviour
+{
+    string html;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.AutomaticDecompression = DecompressionMethods.GZip;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            html = reader.ReadToEnd();
+        }
+        print(html);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
